048b79c0eebae2da8ac21eb4bd2ee7b402c8f6a6
Noime Raymundo <noimerymnd29@gmail.com>
2019-03-29 22:01:50 +0800

    Update ABOUTUS.php

--- ABOUTUS.php
+++ ABOUTUS.php
@@ -8,7 +8,7 @@ include('includes/session.php');
     <link rel="shortcut icon" href="assets/img/logowhite.png" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Magiting | Home</title>
+    <title>Magiting | About Us</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/STYLES.css">
 </head>
