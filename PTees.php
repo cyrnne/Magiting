ccfd758e2504fb57141e36bf4136f13f8b85271c
Noime Raymundo <noimerymnd29@gmail.com>
2019-03-30 13:09:57 +0800

    cart

--- PTees.php
+++ PTees.php
@@ -20,20 +20,19 @@ include('includes/session.php');
 
 <body style="color: rgb(255,255,255);">
     <h1 class="home-heading" style="color: rgb(255,255,255);"><span class="text-center home-heading-upper" style="color: rgb(116,116,116);font-size: 30px;">A Baybayin Movement</span><span class="text-center home-heading-lower" style="font-size: 120px;">MAGITING</span></h1>
-    <nav class="navbar navbar-light navbar-expand-lg bg-white"
-        id="mainNav" style="background-color: #ffffff;">
-        <div class="container-fluid"><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-1"><a href="#" class="blank">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a><a href="#" class="blank">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a><a href="#" class="blank">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a>
-                <ul
-                    class="nav navbar-nav mx-auto" style="margin: 0px;padding: 0px;">
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="index.php">HOME</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="ABOUTUS.PHP">ABOUT US</a></li>
-                    <li class="dropdown nav-item"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">PRODUCTS</a>
-                        <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="PTees.php">Tees</a><a class="dropdown-item" role="presentation" href="PHats.php">Hats</a><a class="dropdown-item" role="presentation" href="PStick.php">Stickers</a></div>
-                    </li>
-                    </ul><?php echo $menuBar; ?></div>
-        </div>
-        </nav>
+    <nav class="navbar navbar-light navbar-expand-lg bg-white" id="mainNav" style="background-color: #ffffff;">
+    <div class="container-fluid"><button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav mx-auto" style="margin: 0px;padding: 0px;">
+                <li role="presentation" class="nav-item"><a href="CART.php" class="nav-link cart" style="width: 35px;height: 35px;margin: 0px;background-image: url('assets/img/cart.png');padding-left: 8px;margin-right: 300px;"></a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="index.php">HOME</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="ABOUTUS.PHP">ABOUT US</a></li>
+                <li class="dropdown nav-item" style="margin-left: 0px;margin-right: 50px;"><a data-toggle="dropdown" aria-expanded="false" href="#" class="dropdown-toggle nav-link" style="margin-right: 65px;">PRODUCTS</a>
+                    <div role="menu" class="dropdown-menu"><a role="presentation" href="PTEES.php" class="dropdown-item">Tees</a><a role="presentation" href="PHATS.php" class="dropdown-item">Hats</a><a role="presentation" href="PSTICKERS.php" class="dropdown-item">Stickers</a></div>
+                </li>
+            </ul><a href="LOGIN.php" class="login">Log In</a><a class="btn btn-light action-button" role="button" href="SIGNUP.php">Sign Up</a></div>
+    </div>
+</nav>
         
         <section class="page-section about-heading">
     <div class="about-heading-content">
