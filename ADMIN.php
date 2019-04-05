df66e961167b6f80cf0c2de2ad6d377f59d57ba1
Noime Raymundo <noimerymnd29@gmail.com>
2019-04-06 06:21:32 +0800

    Update ADMIN.php

--- ADMIN.php
+++ ADMIN.php
@@ -92,7 +92,7 @@ else if ($_REQUEST['action']=="Delete"){
     <link rel="shortcut icon" href="assets/img/logowhite.png" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Magiting | Home</title>
+    <title>Magiting | Admin</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/STYLES.css">
