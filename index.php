5c5af75bce322d0c5f650b329a20aa7c95579aae
Noime Raymundo <noimerymnd29@gmail.com>
2019-04-06 10:58:34 +0800

    Adjusted alignments

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include('includes/session.php');
 
 <body style="color: rgb(255,255,255);">
 <header class="home-header" style="margin-top: -10px;margin-bottom: -10px;"><img class="d-block float-left" src="assets/img/HEADERS/home header.png" style="width: 100%;" /></header>
-    <nav class="navbar navbar-light navbar-expand-lg bg-white" id="mainNav" style="background-color: #ffffff;">
+    <nav class="navbar navbar-light navbar-expand-lg bg-white" id="mainNav" style="background-color: #ffffff; padding-left: 1in;">
     
     <div class="container-fluid"><button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-1">
